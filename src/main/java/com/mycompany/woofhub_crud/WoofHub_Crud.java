bd5786e600ae1b32d30f53be7b65cd718cdaf600
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2023-05-31 16:32:56 -0300

    New crud
    Completo até a aula 03.
    não consegui conectar no banco essa porcaria

---
+++ src/main/java/com/mycompany/woofhub_crud/WoofHub_Crud.java
@@ -0,0 +1,18 @@
+package com.mycompany.woofhub_crud;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author gustavo.j.siebel
+ */
+public class WoofHub_Crud {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
